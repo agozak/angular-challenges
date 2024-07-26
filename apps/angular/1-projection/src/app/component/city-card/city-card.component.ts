ffbe052e10f3b6a0c4cdf6207e99b259ec01cdf2
AgoZak <agozak@gmail.com>
2024-07-26 12:25:11 +0200

    feat: component CityComponent added

--- apps/angular/1-projection/src/app/component/city-card/city-card.component.ts
+++ apps/angular/1-projection/src/app/component/city-card/city-card.component.ts
@@ -1,13 +1,50 @@
 import { Component, OnInit } from '@angular/core';
+import { CityStore } from '../../data-access/city.store';
+import {
+  FakeHttpService,
+  randomCity,
+} from '../../data-access/fake-http.service';
+import { CardType } from '../../model/card.model';
+import { City } from '../../model/city.model';
+import { CardComponent } from '../../ui/card/card.component';
 
 @Component({
   selector: 'app-city-card',
-  template: 'TODO City',
+  template: `
+    <app-card
+      imgSrc="assets/img/city.png"
+      [list]="cities"
+      (delete)="delete($any($event))"
+      (addNewItem)="addNewItem()"
+      customClass="bg-sky-100/50">
+      <ng-template let-value>
+        <div class="flex flex-col">
+          <span>{{ value.name }}</span>
+          <span class="text-xs text-gray-500">{{ value.country }}</span>
+        </div>
+      </ng-template>
+    </app-card>
+  `,
   standalone: true,
-  imports: [],
+  imports: [CardComponent],
 })
 export class CityCardComponent implements OnInit {
-  constructor() {}
+  cities: City[] = [];
+  cardType = CardType.CITY;
 
-  ngOnInit(): void {}
+  constructor(
+    private http: FakeHttpService,
+    private store: CityStore,
+  ) {}
+
+  ngOnInit(): void {
+    this.http.fetchCities$.subscribe((c) => this.store.addAll(c));
+    this.store.cities$.subscribe((c) => (this.cities = c));
+  }
+  addNewItem() {
+    this.store.addOne(randomCity());
+  }
+  delete(city: City) {
+    this.store.deleteOne(city.id);
+  }
 }
